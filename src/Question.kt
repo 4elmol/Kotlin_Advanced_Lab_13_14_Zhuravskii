b6bea61593297dd46893f3be7602415e672930d7
4elmol <ne4eLoDoIMoLoVeK@yandex.ru>
2026-02-04 14:26:43 +0300

    Use a data class

--- src/Question.kt
+++ src/Question.kt
@@ -15,7 +15,7 @@
 //    val answer: Int,
 //    val difficulty: String
 //)
-class Question<T>(
+data class Question<T>(
     val questionText: String,
     val answer: T,
     val difficulty: Difficulty
